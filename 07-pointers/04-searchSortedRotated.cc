4153c23848b81bed55d0104714d25a01808fc9e7
Deepak Aggarwal <daggarwal77@codingblocks.com>
2018-06-27 09:46:12 +0530

    Search Rotated Sorted Array Fixed

--- 07-pointers/04-searchSortedRotated.cc
+++ 07-pointers/04-searchSortedRotated.cc
@@ -8,7 +8,9 @@ void inputArr(int arr[], int n) {
     }
 }
 
-int searchRotated(int arr[], int n, int key) {
+int searchRotated0(int arr[], int n, int key) {
+    // THE FOLLOWING PROGRAM CONTAINS BUG. FIXED SOLUTION IS NEXT TO THIS FUNCTION
+    // viz searchRotated. THE SOLUTION WAS EXPECTED AS HOMEWORK :)
     int be = 0;
     int en = n - 1;
 
@@ -26,6 +28,7 @@ int searchRotated(int arr[], int n, int key) {
             }
         }
         else {
+            // left is unsorted
             if (key >= arr[be] or key < arr[mid]) {
                 en = mid - 1 ;
             }
@@ -37,6 +40,35 @@ int searchRotated(int arr[], int n, int key) {
     return -1;
 }
 
+
+int searchRotated(int arr[], int n, int key) {
+    int be = 0;
+    int en = n - 1;
+    while (be <= en) {
+        int mid = (be + en) / 2;
+        if (arr[mid] == key) return mid;
+
+        if (arr[mid] > arr[en]){
+            // right is unsorted
+            if (key < arr[mid] && key >= arr[be]){
+                en = mid - 1;
+            }
+            else {
+                be = mid + 1;
+            }
+        }
+        else {
+            // left is unsorted
+            if (key > arr[mid] && key <= arr[en]){
+                be = mid + 1;
+            }
+            else {
+                en = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
 
 int lowerBound(int arr[], int n, int key) {
     int be = 0;
@@ -60,7 +92,7 @@ int main() {
     int n; cin >> n;
     inputArr(arr, n);
     int key; cin >> key;
-    // int ans = searchRotated(arr, n, key);
-    int ans = lowerBound(arr, n, key);
+    int ans = searchRotated(arr, n, key);
+    // int ans = lowerBound(arr, n, key);
     cout << ans;
 }
